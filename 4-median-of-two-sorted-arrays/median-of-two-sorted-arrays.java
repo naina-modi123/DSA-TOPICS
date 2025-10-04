7c6bea2c41917411d0e4e5dc46d66f7c27d29043
Naina Modi <nainamodi123@gmail.com>
2025-10-05 00:02:38 +0530

    Time: 1 ms (100.00%) | Memory: 45.9 MB (85.35%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -4,8 +4,6 @@ class Solution {
         int n2 = nums2.length;
         int[] nums3 = new int[n1 + n2];
         int i = 0, j = 0, k = 0;
-        
-        // Merge arrays
         while(i < n1 && j < n2) {
             if(nums1[i] < nums2[j]) {
                 nums3[k++] = nums1[i++];
@@ -15,8 +13,6 @@ class Solution {
         }
         while(i < n1) nums3[k++] = nums1[i++];
         while(j < n2) nums3[k++] = nums2[j++];
-        
-        // Find median
         int total = n1 + n2;
         if(total % 2 == 0) {
             return (nums3[total/2 - 1] + nums3[total/2]) / 2.0;
