3d1f7f3ac5cde888646cf35a01b119ff54704057
Naina Modi <nainamodi123@gmail.com>
2025-10-05 19:11:40 +0530

    Time: 6 ms (91.42%) | Memory: 58.3 MB (92.98%) - LeetSync

---
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans;
+    public int maxSumBST(TreeNode root) {
+        ans = 0;
+        dfs(root);
+        return ans;
+    }
+    public int[] dfs(TreeNode root){
+        // base case
+        if(root == null){
+            return new int[]{Integer.MAX_VALUE,Integer.MIN_VALUE,0};
+        }
+        int [] leftSubtree = dfs(root.left);
+        int[] rightSubtree = dfs(root.right);
+        // checking if the current bt is bst
+
+        if(root.val > leftSubtree[1] && root.val < rightSubtree[0]){
+            int currSum = leftSubtree[2]+rightSubtree[2]+root.val;
+            ans = Math.max(ans , currSum);
+            int minVal = Math.min(root.val , leftSubtree[0]);
+            int maxVal = Math.max(root.val , rightSubtree[1]);
+            return new int[]{minVal,maxVal,currSum};
+        }
+        int maxSum = Math.max(leftSubtree[2],rightSubtree[2]);
+        return new int[]{Integer.MIN_VALUE,Integer.MAX_VALUE,maxSum};
+    }
+}
