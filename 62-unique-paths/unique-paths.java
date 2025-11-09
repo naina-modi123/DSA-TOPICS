0770ba3a5ffc952edf86e2504987016d905e321a
Naina Modi <nainamodi123@gmail.com>
2025-11-09 22:38:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (5.15%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,20 +1,14 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] memo = new int[m][n];
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                memo[i][j] = -1;
-        return countPaths(m - 1, n - 1, memo);
-    }
+        int[][] dp = new int[m][n];
+        for(int i=0;i<m;i++) dp[i][n-1] = 1;
+        for(int j=0;j<n;j++) dp[m-1][j] = 1;
 
-    public int countPaths(int i, int j, int[][] memo) {
-        if (i == 0 && j == 0)
-            return 1;
-        if (i < 0 || j < 0)
-            return 0;
-        if (memo[i][j] != -1)
-            return memo[i][j];
-        memo[i][j] = countPaths(i - 1, j, memo) + countPaths(i, j - 1, memo);
-        return memo[i][j];
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+        return dp[0][0];
     }
 }
