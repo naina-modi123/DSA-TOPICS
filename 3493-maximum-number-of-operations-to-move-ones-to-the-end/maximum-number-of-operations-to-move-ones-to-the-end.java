c011ef4d5497d37dbb1d6156e1b245544e1df6aa
Naina Modi <nainamodi123@gmail.com>
2025-11-13 08:26:44 +0530

    Time: 7 ms (78.52%) | Memory: 47.9 MB (5.19%) - LeetSync

---
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxOperations(String s) {
+        int ones = 0;
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '1') {
+                ones++;
+            } else {
+                if (i == n - 1 || s.charAt(i + 1) == '1') {
+                    ans += ones;
+                }
+            }
+        }
+        return ans;
+    }
+}
