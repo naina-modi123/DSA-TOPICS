b1cdb5cd08506ec911bab53d84e4ba56e98bdc1b
Naina Modi <nainamodi123@gmail.com>
2025-11-25 11:50:53 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (9.61%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-        return (double)Math.pow(x,n);
+        long N = n;
+        if(N < 0){
+            N *= -1;
+            x = 1/x;
+        }
+        return helper(x, N);
+    }
+    public double helper(double x , long n){
+        if(n == 0) return 1;
+        double subans =  helper(x, n/2);
+        if(n % 2 == 0){
+            return subans*subans;
+        }
+        return subans*subans * x;
     }
 }
