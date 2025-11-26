50d583bef77f3101d919003379ffeb662daaa9c0
Naina Modi <nainamodi123@gmail.com>
2025-11-26 07:28:02 +0530

    Time: 10 ms (5.48%) | Memory: 44.2 MB (5.56%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<String> prepareTheConfig(boolean[][] board, int n){
+        List<String> config = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            String ans = "";
+            for(int j=0;j<n;j++){
+                ans += board[i][j] ? 'Q' : '.';
+            }
+            config.add(ans);
+        }
+        return config;
+    }
+
+    public boolean isSafe(int row, int col, boolean[][] board, int n){
+        for(int r = row-1,c = col-1; r >= 0 && c >= 0; r--,c--){
+            if(board[r][c]) return false;
+        }
+        for(int r = row-1; r >= 0; r--){
+            if(board[r][col]) return false;
+        }
+        for(int r = row-1,c=col+1; r >= 0 && c<n; r--,c++){
+            if(board[r][c]) return false;
+        }
+        return true;
+    }
+
+    public void helper(int row, boolean[][] board, int n, List<List<String>> ans){
+        if(row == n){
+            ans.add(prepareTheConfig(board, n));
+            return;
+        }
+
+        for(int col = 0; col < n; col++){
+            if(isSafe(row, col, board, n)){
+                board[row][col] = true;
+                helper(row+1, board, n, ans);
+                board[row][col] = false;
+            }
+        }
+    }
+    public int totalNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+        boolean[][] board = new boolean[n][n];
+        helper(0, board, n, ans);
+        return ans.size();
+        
+    }
+}
