189092aa01bafb5f7af1ed41dfb72cdce2309fa3
Naina Modi <nainamodi123@gmail.com>
2025-11-30 09:57:51 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (17.70%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,47 +1,42 @@
 class Solution {
+    public int firstOcc(int[] nums, int x){
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
+        while(low <= high){
+            int mid = low+(high - low)/2;
+            if(nums[mid] == x){
+                ans = mid;
+                high = mid-1;
+            }else if(nums[mid] > x){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+    public int lastOcc(int[] nums, int x){
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
+        while(low <= high){
+            int mid = low+(high-low)/2;
+            if(nums[mid] == x){
+                ans = mid;
+                low = mid+1;
+            }else if(nums[mid] > x){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
     public int[] searchRange(int[] nums, int target) {
-        int first = lb(nums, target);
-        int last = ub(nums, target);
-        return new int[]{first, last};
-        
-    }
-    private int lb(int[] nums, int target){
-        int n = nums.length;
-        int low = 0, high = n-1;
-        int ans1 = -1;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] == target){
-                ans1 = mid;
-                high = mid-1;
-            }
-            else if(nums[mid] < target){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
-        }
-        return ans1;
-    }
-    private int ub(int[] nums, int target){
-        int n = nums.length;
-        int low = 0, high = n-1;
-        int ans2 = -1;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] == target){
-                ans2 = mid;
-                low = mid+1;
-            
-            }
-            else if(nums[mid] < target){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
-        }
-        return ans2;
+        int[] ans = new int[2];
+        ans[0] = firstOcc(nums, target);
+        ans[1] = lastOcc(nums, target);
+        return ans;
     }
 }
