a522e97d6555534e7554179cc17033b9c8efa814
Naina Modi <nainamodi123@gmail.com>
2025-12-02 09:37:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (12.36%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,17 +1,37 @@
 class Solution {
-    public int climb_memo(int n, int[] dp){
+    private int cs_rec(int n){
+        if(n == 0 || n == 1) return 1;
+        return cs_rec(n-1)+cs_rec(n-2);
+
+    }
+    private int cs_memo(int n, int[] dp){
+        if(n == 0 || n == 1) return 1;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = cs_memo(n-1, dp)+cs_memo(n-2, dp);
+    }
+    private int cs_tab(int n, int[] dp){
+        if(n == 0 || n == 1) return 1;
         dp[0] = 1;
         dp[1] = 1;
-        if(dp[n] !=  -1){
-            return dp[n];
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
         }
-        dp[n] = climb_memo(n-1, dp)+climb_memo(n-2, dp);
         return dp[n];
-        
+    }
+    private int cs_spo(int n){
+        int a = 1;
+        int b = 1;
+        for(int i=2;i<= n;i++){
+            int c = a+b;
+            a = b;
+            b = c;
+        }
+        return b;
     }
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
+        int[] dp = new int [n+1];
         Arrays.fill(dp, -1);
-        return climb_memo(n, dp);
+        return cs_spo(n);
+        
     }
 }
