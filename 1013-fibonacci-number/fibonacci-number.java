550dd8635ca70c5b79dd604211dd744ac7ef6af4
Naina Modi <nainamodi123@gmail.com>
2025-12-02 08:14:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (11.62%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -9,9 +9,29 @@ class Solution {
         dp[n] = fib_memo(n-1, dp)+fib_memo(n-2, dp);
         return dp[n];
     }
+    public int fib_tab(int n, int[] dp){
+        if(n == 0 || n == 1) return n;
+        dp[0] = 0;
+        dp[1] = 1; 
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }  
+        return dp[n]; 
+    }
+    public int fib_spo(int n){
+        if(n == 0 || n == 1) return n;
+        int a = 0;
+        int b = 1;
+        for(int i=2;i<=n;i++){
+            int c = a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
     public int fib(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
-        return fib_memo(n, dp);
+        return fib_spo(n);
     }
 }
