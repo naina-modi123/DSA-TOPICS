3b7fa78600cf7a5ac5a9eab3f0dd02b7b2a65644
Naina Modi <nainamodi123@gmail.com>
2025-12-02 07:52:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (11.62%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,17 @@
 class Solution {
+    public int fib_memo(int n, int[] dp){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        dp[n] = fib_memo(n-1, dp)+fib_memo(n-2, dp);
+        return dp[n];
+    }
     public int fib(int n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        return fib(n-1) + fib(n-2);
-        
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return fib_memo(n, dp);
     }
 }
