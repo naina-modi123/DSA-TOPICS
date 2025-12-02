224d272322a111602afe11a7833696d0eacc6890
Naina Modi <nainamodi123@gmail.com>
2025-12-02 23:19:13 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (30.63%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,14 +1,27 @@
-public class Solution {
+class Solution {
+    private int up_rec(int row, int col, int m, int n){
+        if(row >= m || col >= n) return 0;
+        if(row == m-1 && col ==  n-1) return 1;
+        int rightPaths = up_rec(row, col+1, m, n);
+        int downPaths = up_rec(row+1, col, m, n);
+        return rightPaths+downPaths; 
+    }
+    private int up_memo(int row, int col, int m, int n, int[][] dp){
+        if(row >= m || col >= n) return 0;
+        if(row == m-1 && col ==  n-1) return 1;
+        if(dp[row][col] != -1) return dp[row][col];
+        int rightPaths = up_memo(row, col+1, m, n, dp);
+        int downPaths = up_memo(row+1, col, m, n, dp);
+        return dp[row][col] = rightPaths+downPaths; 
+    }
+    
+
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++) dp[i][n-1] = 1;
-        for(int j=0;j<n;j++) dp[m-1][j] = 1;
-
-        for(int i=m-2;i>=0;i--){
-            for(int j=n-2;j>=0;j--){
-                dp[i][j] = dp[i+1][j] + dp[i][j+1];
-            }
+        for(int[] x : dp){
+            Arrays.fill(x, -1);
         }
-        return dp[0][0];
+        return up_memo(0, 0, m, n, dp);
+        
     }
 }
