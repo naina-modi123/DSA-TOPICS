f8752d45016eb4c4aaa7ea9869c1271e368454c4
Naina Modi <nainamodi123@gmail.com>
2025-12-02 09:17:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (12.36%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,12 +1,17 @@
 class Solution {
-    public int climbStairs(int n) {
-        if (n <= 2) return n;
-        int []dp = new int[n+1];
+    public int climb_memo(int n, int[] dp){
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
-        for(int i=3;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+        if(dp[n] !=  -1){
+            return dp[n];
         }
+        dp[n] = climb_memo(n-1, dp)+climb_memo(n-2, dp);
         return dp[n];
+        
+    }
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return climb_memo(n, dp);
     }
 }
