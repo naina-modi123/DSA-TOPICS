def22f503d258a5609b6921b4257054076d8896c
Naina Modi <nainamodi123@gmail.com>
2025-12-04 18:14:26 +0530

    Time: 18 ms (89.97%) | Memory: 58 MB (5.23%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -25,14 +25,44 @@ class Solution {
             return dp[i][j] = Math.max(ans1, ans2);
         }
     }
+    private int lcs_tab(int n, int m, String t1, String t2, int[][] dp){
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(t1.charAt(i) == t2.charAt(j)){
+                    dp[i][j] = dp[i+1][j+1]+1;
+                }else{
+                    dp[i][j] = Math.max(dp[i][j+1], dp[i+1][j]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+    private int lcs_spo(String t1, String t2){
+        int n = t1.length();
+        int m = t2.length();
+        int[] curr = new int[m+1];
+        int[] next = new int[m+1];
+        for(int i= n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(t1.charAt(i) == t2.charAt(j)){
+                    curr[j] = next[j+1]+1;
+                }
+                else{
+                    curr[j] = Math.max(curr[j+1], next[j]);
+                }
+            }
+            next = curr.clone();
+        }
+        return curr[0];
+    }
     public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();
         int[][] dp = new int[n+1][m+1];
-        for(int[] a: dp){
-            Arrays.fill(a, -1);
-        }
-        return lcs_memo(0, 0, text1, text2, dp);
+        //for(int[] a: dp){
+            //Arrays.fill(a, -1);
+        //}
+        return lcs_spo(text1, text2);
     }
     
 }
