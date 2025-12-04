72aac7b339c36c5d3c40bd4c962a88aea30a0106
Naina Modi <nainamodi123@gmail.com>
2025-12-04 08:14:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (19.31%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,24 +1,38 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n = obstacleGrid.length;
-        int m = obstacleGrid[0].length;
-        int[][] memo = new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                memo[i][j] = -1;
-            }
-        }
-        return helper(obstacleGrid,0,0,memo);
-        
-    }
-    private int helper(int[][] obstacleGrid, int i, int j, int[][] memo){
-        int n = obstacleGrid.length;
-        int m = obstacleGrid[0].length;
-        if(i >= n || j >= m || obstacleGrid[i][j] == 1){
+    private int upo_rec(int row, int col, int m, int n, int[][] grid){
+        if(row >= m || col >= n || grid[row][col] == 1){
             return 0;
         }
-        if(memo[i][j] != -1) return memo[i][j];
-        if(i == n-1 && j == m-1) return 1;
-        return memo[i][j] = helper(obstacleGrid,i+1,j,memo)+helper(obstacleGrid,i,j+1,memo);
+        if(row == m-1 && col == n-1){
+            return 1;
+        }
+        int rightPaths = upo_rec(row, col+1, m, n, grid);
+        int downPaths = upo_rec(row+1, col, m, n, grid);
+
+        return rightPaths+downPaths;
+    }
+    private int upo_memo(int row, int col, int m, int n, int[][] grid, int[][] dp){
+        if(row >= m || col >= n || grid[row][col] == 1){
+            return 0;
+        }
+        if(row == m-1 && col == n-1){
+            return 1;
+        }
+        if(dp[row][col] != -1) return dp[row][col];
+        int rightPaths = upo_memo(row, col+1, m, n, grid, dp);
+        int downPaths = upo_memo(row+1, col, m, n, grid, dp);
+
+        return dp[row][col] = rightPaths+downPaths;
+    }
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for(int[] x: dp){
+            Arrays.fill(x, -1);
+        }
+        return upo_memo(0, 0, m, n, obstacleGrid, dp);
+        
     }
 }
