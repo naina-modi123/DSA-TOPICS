365bffe3666be2165edd2fabce0930ffc6f2d3c5
Naina Modi <nainamodi123@gmail.com>
2025-12-05 17:50:11 +0530

    Time: 3 ms (95.85%) | Memory: 47 MB (41.33%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,24 +1,41 @@
 class Solution {
+    private int mD_memo(String w1, String w2, int n, int m, int[][] dp){
+        if(n == 0) return m;
+        if(m == 0) return n;
+        if(dp[n][m] != -1) return dp[n][m];
+        if(w1.charAt(n-1) == w2.charAt(m-1)){
+            return dp[n][m] = mD_memo(w1, w2, n-1, m-1, dp);
+        }
+        else{
+            int insert = mD_memo(w1, w2, n, m-1, dp);
+            int replace = mD_memo(w1, w2, n-1, m-1, dp);
+            int delete = mD_memo(w1, w2, n-1, m, dp);
+            return dp[n][m] = Math.min(insert, Math.min(replace, delete)) + 1;
+        }
+    }
+    private int mD_rec(String w1, String w2, int n, int m){
+        if(n == 0) return m;
+        if(m == 0) return n;
+        if(w1.charAt(n-1) == w2.charAt(m-1)){
+            return mD_rec(w1, w2, n-1, m-1);
+        }
+        else{
+            int insert = mD_rec(w1, w2, n, m-1);
+            int replace = mD_rec(w1, w2, n-1, m-1);
+            int delete = mD_rec(w1, w2, n-1, m);
+            return Math.min(insert, Math.min(replace, delete)) + 1;
+        }
+    }
     public int minDistance(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
+        int n = word1.length();
+        int m = word2.length();
 
-        int[][] dp = new int[m+1][n+1];  
-        for(int i=0;i<=m;i++){
-            dp[i][0] = i;
+        int[][] dp = new int[n+1][m+1];
+        for(int[] a : dp){
+            Arrays.fill(a, -1);
         }
-        for(int j=0;j<=n;j++){
-            dp[0][j] = j;
-        }
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(word1.charAt(i-1) == word2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
-                }else{
-                    dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]));
-                }
-            }
-        }
-        return dp[m][n];
+        
+        return mD_memo(word1, word2, n, m, dp);
+        
     }
 }
