d7f5d826256452abd0a3f27802a3343c674d92d7
Naina Modi <nainamodi123@gmail.com>
2025-12-06 09:32:40 +0530

    Time: 263 ms (15.98%) | Memory: 236.6 MB (10.72%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,17 +1,40 @@
 class Solution {
+    private int lis_rec(int[] nums, int idx, int prev){
+        if(idx == nums.length){
+            return 0;
+        }
+        int ans = 0;
+        if(prev == -1 || nums[idx] > nums[prev]){
+            int take = lis_rec(nums, idx+1, idx) + 1;
+            ans = Math.max(ans, take);
+        }
+        int nonTake = lis_rec(nums, idx+1, prev);
+        ans = Math.max(ans, nonTake);
+
+        return ans;
+    }
+    private int lis_memo(int[] nums, int idx, int prev, int[][] dp){
+        if(idx == nums.length){
+            return 0;
+        }
+        if(prev != -1 && dp[prev][idx] != -1) return dp[prev][idx];
+        int ans = 0;
+        if(prev == -1 || nums[idx] > nums[prev]){
+            int take = lis_memo(nums, idx+1, idx, dp) + 1;
+            ans = Math.max(ans, take);
+        }
+        int nonTake = lis_memo(nums, idx+1, prev, dp);
+        ans = Math.max(ans, nonTake);
+
+        return (prev != -1) ? dp[prev][idx] = ans : ans;
+    }
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-        int ans = 1;
-        for(int idx = 1;idx<n;idx++){
-            for(int prev = 0;prev<idx;prev++){
-                if(nums[prev] < nums[idx]){
-                    dp[idx] = Math.max(dp[idx], dp[prev]+1);
-                }
-            }
-            ans = Math.max(ans, dp[idx]);
+        int[][] dp = new int[n+1][n+1];
+        for(int[] a: dp){
+            Arrays.fill(a, -1);
         }
-        return ans;
+        return lis_memo(nums, 0, -1, dp);
+        
     }
 }
