6db7e1d78c8775743b167ccb2f6094f71f08c3bc
Naina Modi <nainamodi123@gmail.com>
2025-12-26 20:59:05 +0530

    Time: 11 ms (75.86%) | Memory: 47.2 MB (20.69%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int bestClosingTime(String customers) {
+        int n = customers.length();
+        int penalty = 0;
+        for (char c : customers.toCharArray()) {
+            if (c == 'N') penalty++;
+        }
+        
+        int minPenalty = penalty;
+        int ans = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (customers.charAt(i) == 'Y')
+                penalty--;   
+            else
+                penalty++;  
+            
+            if (penalty < minPenalty) {
+                minPenalty = penalty;
+                ans = i + 1;
+            }
+        }
+        
+        return ans;
+    }
+}
