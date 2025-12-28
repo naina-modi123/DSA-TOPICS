af23cd68419d04c3942d63d8d31e1849f05cb749
Naina Modi <nainamodi123@gmail.com>
2025-12-28 16:36:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (36.44%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,12 +1,24 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // Brute force
-        int j=0;
-        for(int i=m;i<m+n;i++){
-            nums1[i] = nums2[j];
-            j++;
+        //better force
+        int nums3[] = new int[m+n];
+        int start1 = 0, start2 = 0, start3 = 0;
+        while(start1 < m && start2 < n){
+            if(nums1[start1] <= nums2[start2]){
+                nums3[start3++] = nums1[start1++];
+            }
+            else{
+                nums3[start3++] = nums2[start2++];
+            }
         }
-        Arrays.sort(nums1);
-        System.out.println(nums1);
+        while(start1 < m){
+            nums3[start3++] = nums1[start1++];
+        }
+        while(start2 < n){
+            nums3[start3++] = nums2[start2++];
+        }
+        for(int i=0;i<m+n;i++){
+            nums1[i] = nums3[i];
+        }
     }
 }
