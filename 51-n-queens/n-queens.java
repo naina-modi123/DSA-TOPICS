c4ab8e98a15ed2793d51f4ac84c075345861e7c4
Naina Modi <nainamodi123@gmail.com>
2026-01-01 11:51:04 +0530

    Time: 6 ms (19.54%) | Memory: 46.9 MB (14.73%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -4,45 +4,54 @@ class Solution {
         for(int i=0;i<n;i++){
             String ans = "";
             for(int j=0;j<n;j++){
-                ans += board[i][j] ? 'Q' : '.';
+                if(board[i][j] == true){
+                    ans += "Q";
+                }else{
+                    ans += ".";
+                }
             }
             config.add(ans);
         }
         return config;
     }
-
-    public boolean isSafe(int row, int col, boolean[][] board, int n){
-        for(int r = row-1,c = col-1; r >= 0 && c >= 0; r--,c--){
-            if(board[r][c]) return false;
+    public boolean isSafePos(int row, int col, boolean[][] board, int n){
+        for(int r = row-1,c = col-1;r>=0 && c>=0;r--,c--){
+            if(board[r][c] == true){
+                return false;
+            }
         }
-        for(int r = row-1; r >= 0; r--){
-            if(board[r][col]) return false;
+        for(int r = row-1;r >= 0;r--){
+            if(board[r][col] == true){
+                return false;
+            }
         }
-        for(int r = row-1,c=col+1; r >= 0 && c<n; r--,c++){
-            if(board[r][c]) return false;
+        for(int r = row-1, c = col+1; r>=0 && c<n;r--,c++){
+            if(board[r][c] == true){
+                return false;
+            }
         }
         return true;
     }
-
-    public void helper(int row, boolean[][] board, int n, List<List<String>> ans){
+    private void helper(int row, boolean[][] board, int n, List<List<String>> ans){
         if(row == n){
-            ans.add(prepareTheConfig(board, n));
+            List<String> subAns = prepareTheConfig(board, n);
+            ans.add(new ArrayList<>(subAns));
             return;
         }
-
-        for(int col = 0; col < n; col++){
-            if(isSafe(row, col, board, n)){
+        for(int col = 0;col<n;col++){
+            if(isSafePos(row, col, board, n)){
                 board[row][col] = true;
                 helper(row+1, board, n, ans);
                 board[row][col] = false;
             }
+
         }
     }
-
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
         boolean[][] board = new boolean[n][n];
         helper(0, board, n, ans);
         return ans;
+        
     }
 }
