dd7c42ea3521ee19248613031641b4b7d979ba56
Naina Modi <nainamodi123@gmail.com>
2026-01-06 11:36:55 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (32.32%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -15,14 +15,13 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        while(root != null && root.val != val){
-            if(val < root.val){
-                root = root.left;
-            }
-            else{
-                root = root.right;
-            }
+        if(root == null) return null;
+        if(root.val == val) return root;
+        if(root.val > val){
+            return searchBST(root.left, val);
+        }else{
+            return searchBST(root.right, val);
         }
-        return root;
+        
     }
 }
