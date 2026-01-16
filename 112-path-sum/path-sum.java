6649afe2f7d7e5894e122b5ab1471a45a3adeb4e
Naina Modi <nainamodi123@gmail.com>
2026-01-16 14:50:03 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (19.70%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -17,11 +17,10 @@ class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root == null) return false;
         if(root.left == null && root.right == null){
-            return targetSum == root.val;
+            if(root.val == targetSum) return true;
         }
-        return hasPathSum(root.left , targetSum-root.val) ||
-               hasPathSum(root.right , targetSum-root.val);
-
-        
+        boolean lh = hasPathSum(root.left, targetSum - root.val);
+        boolean rh = hasPathSum(root.right, targetSum - root.val);
+        return lh || rh;
     }
 }
