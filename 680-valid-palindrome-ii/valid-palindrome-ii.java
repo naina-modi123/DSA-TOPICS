a5140ad78338ea618baa5bec7cb6710ddc6c880e
Naina Modi <nainamodi123@gmail.com>
2026-02-01 19:59:52 +0530

    Time: 4 ms (99.51%) | Memory: 47.9 MB (22.84%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    private boolean isPalindrome(String s, int i, int j){
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int i=0, j = s.length() - 1;
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                if(isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1)){
+                    return true;
+                }
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+        
+    }
+}
