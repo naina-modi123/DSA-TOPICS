a12ba4931edfee93b7104a99fa4d6af8ad84b4c8
Naina Modi <nainamodi123@gmail.com>
2026-02-28 10:59:03 +0530

    Time: 41 ms (50.00%) | Memory: 42.1 MB (100.00%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+        long ans = 0;
+        int bitLength = 0;
+        int MOD = 1000000007;
+
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) {
+                bitLength++;
+            }
+            
+            ans = ((ans << bitLength) % MOD + i) % MOD;
+        }
+
+        return (int) ans;
+    }
+}
