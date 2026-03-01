3b554e0086698c2c2f3010dad97d2d05c9ede46a
Naina Modi <nainamodi123@gmail.com>
2026-03-01 11:09:57 +0530

    Time: 7 ms (60.95%) | Memory: 47.3 MB (34.40%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,43 +1,32 @@
-import java.util.*;
-
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-        for (int i = 0; i < numCourses; i++) {
-            adj.add(new ArrayList<>());
+        List<List<Integer>> adjList = new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adjList.add(new ArrayList<>());
         }
-
         int[] indegree = new int[numCourses];
-
-        for (int[] p : prerequisites) {
-            int course = p[0];
-            int pre = p[1];
-            adj.get(pre).add(course);
-            indegree[course]++;
+        for(int prerequisite[] : prerequisites){
+            adjList.get(prerequisite[0]).add(prerequisite[1]);
+            indegree[prerequisite[1]]++;
         }
-
+        ArrayList<Integer> ans = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i] == 0){
                 q.add(i);
             }
         }
-
-        int count = 0;
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            count++;
-
-            for (int neigh : adj.get(node)) {
-                indegree[neigh]--;
-                if (indegree[neigh] == 0) {
-                    q.add(neigh);
+        while(q.size() > 0){
+            int node = q.remove();
+            ans.add(node);
+            for(int nbr : adjList.get(node)){
+                indegree[nbr]--;
+                if(indegree[nbr] == 0){
+                    q.add(nbr);
                 }
             }
         }
-        return count == numCourses;
+        if(ans.size() == numCourses) return true;
+        else return false;
     }
 }
