cdce3be03ed4cc89243573409a4c051dde625ceb
Naina Modi <nainamodi123@gmail.com>
2026-03-19 19:02:38 +0530

    Add sortedSquares method for sorted array squares
    Implement sortedSquares method to return squares of sorted array.

---
+++ Squares-of-a-sorted-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int left = 0, right = n-1;
+        int i= 0;
+        int[] ans = new int[n];
+        while(left <= right){
+            int leftSq = nums[left] * nums[left];
+            int rightSq = nums[right] * nums[right];
+
+            if(leftSq > rightSq){
+                ans[i] = leftSq;
+                left++;
+            }else{
+                ans[i] = rightSq;
+                right--;
+            }
+            i--;
+        }
+        return ans;
+    }
+}
