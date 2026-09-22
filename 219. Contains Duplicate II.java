dbfa175ebee0e429477ab52abacf4a2118ea08d0
Naina Modi <nainamodi123@gmail.com>
2026-09-22 19:26:03 +0530

    Create 219. Contains Duplicate II.java

---
+++ 219. Contains Duplicate II.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(map.containsKey(nums[i])){
+                int prevIdx = map.get(nums[i]);
+                if((i-prevIdx) <= k){
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
